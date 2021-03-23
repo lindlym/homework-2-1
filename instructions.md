19338ff5e3e718ba354dc21f0a04b8cb8c9a4806
Matt Lindly <matt@arrayschool.com>
2021-03-23 17:16:14 -0600

    Homework is ready for work.

---
+++ instructions.md
@@ -0,0 +1,3 @@
+# Your Task
+
+Your task is to figure out why clicking the button does not make the modal appear on the main page. You will have to change anything you need until the modal appears!
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import appModal from './Modal';
+
+function App() {
+  return (
+    <main>
+      <h1>Welcome to your application!</h1>
+      <appModal />
+    </main>
+  );
+}
+
+export default App;
